d6874874839c056d024b9d6ae3c791d937cd3a1b
Ptyg <nawkking@gmail.com>
2025-07-10 21:50:01 +0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('695F6C6F76655F796F750A');
+}
