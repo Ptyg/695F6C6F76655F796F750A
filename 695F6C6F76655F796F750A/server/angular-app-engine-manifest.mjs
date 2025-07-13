f4c0ee37d91d1f5c969a7e7f330c3e70303f73e0
Ptyg <nawkking@gmail.com>
2025-07-13 22:11:50 +0300

    Auto-generated commit

---
+++ 695F6C6F76655F796F750A/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://ptyg.github.io/695F6C6F76655F796F750A',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
